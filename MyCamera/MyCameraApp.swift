9df4d2a93c65f792a1ad31ace6fcacd47a2187f8
najp-rm016 <ryuichi.mochizuki@nippon-antenna.co.jp>
2024-02-08 23:36:39 -0800

    Initial Commit

---
+++ MyCamera/MyCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCameraApp.swift
+//  MyCamera
+//
+//  Created by NSPC201admin on 2024/02/08.
+//
+
+import SwiftUI
+
+@main
+struct MyCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
